69a967f8bef8a0c5c67be9848bd83c8dd9f58e33
Kamba56 <tomajohn316@yahoo.com>
2022-11-26 07:58:34 +0100

    Add model config

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,10 @@
 class Like < ApplicationRecord
+  belongs_to :post
+  belongs_to :user
+
+  private
+
+  def likes_counter
+    post.increment!(:likes_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_many :likes
+  has_many :comments
+
+  def recent_comments
+    Comment.limit(5).order(created_at: :desc)
+  end
+
+  private
+
+  def post_counter
+    user.increment!(:posts_counter)
+  end
 end
