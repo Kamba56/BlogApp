449b074592b5461ac91dc1165b5676fe6be32298
Kamba56 <tomajohn316@yahoo.com>
2022-12-02 12:20:29 +0100

    Add users controller and path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'users#index'
+  get 'users', to: 'users#index'
+  get 'users/:id', to: 'userd#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
