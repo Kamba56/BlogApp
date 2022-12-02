e12ffc9fe13c4d02cd7b0a99ca469ba7d8a88a98
Kamba56 <tomajohn316@yahoo.com>
2022-12-02 21:07:26 +0100

    Add test for user controller and post controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,4 +1,34 @@
 require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe 'Users', type: %w[request] do
+  before :each do
+    @user = User.new(
+      name: 'Toma',
+      photo: 'http://great.com/org.pjg',
+      bio: 'Full-stack developer',
+      posts_counter: 0
+    )
+    @user.save
+  end
+
+  describe 'GET /index' do
+    it 'renders index template' do
+      get '/users/'
+      expect(response.status).to eq(200)
+      expect(response).to render_template('index')
+    end
+  end
+
+  describe 'GET /show' do
+    it 'renders the show template' do
+      get "/users/#{@user.id}"
+      expect(response.status).to eq(200)
+      expect(response).to render_template('show')
+      expect(response.body).to include('Displays specific user')
+    end
+  end
+end
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
