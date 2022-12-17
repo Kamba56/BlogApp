c9017f2f89b30e97c3be6245a6d1890d0aa4c06c
Kamba56 <tomajohn316@yahoo.com>
2022-12-17 19:22:53 +0100

    Finish devise form

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,18 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  before_validation :default_values
+
   validates :name, presence: true
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
     posts.limit(3).order(created_at: :desc)
+  end
+
+  private
+
+  def default_values
+    self.posts_counter = 0
   end
 end
